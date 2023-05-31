aa00e01cd8bf2515acc2b6e60852a0a8140af09c
prithvijedipatil <prithvipatil09@gmail.com>
2023-05-31 13:34:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'an Victoria Memorial', 'a xTajmahal'];
+
+function tri(name){
+	 return name.replace(/^(a |the |an )/i,'');
+  
+ }
+
+ let a= touristSpots.sort((a,b) => tri(a)>tri(b) ? 1 : -1 );
+ console.log(a);
+
+
 
